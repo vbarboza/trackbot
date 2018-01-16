8ebcf5ff63e82170beea48c725db38d7a6ec2b33
Vinicius Barboza <viniciusdearaujob@gmail.com>
2018-01-16 11:35:06 -0200

    Returning Hello, world! when no args passed.

--- index.py
+++ index.py
@@ -23,6 +23,7 @@ c = Chatbot()
 # Simple reponses to GET and POST
 @app.route('/', methods=['GET', 'POST'])
 def webhook():
+
     if request.method == 'POST':
         # Respond to POST messages
         try:
@@ -35,18 +36,18 @@ def webhook():
                        'message':{'text':c.get_response(text)}}
             # Send a response
             r = requests.post(FB_GRAPH + token, json=payload)
+            return r
         except Exception as e:
             # CRASH
             print(traceback.format_exc())
-                        
     elif request.method == 'GET':
-        # Respond to GET authentication
-        if request.args.get('hub.verify_token') == os.environ.get('FB_VERIFY_TOKEN'):
-            # Success
-            return request.args.get('hub.challenge')
-        # Failure
-        return 'Failed!'
-    return 'Hello, world!'
+        if request.args:
+            if request.args.get('hub.verify_token') == os.environ.get('FB_VERIFY_TOKEN'):
+                # Success
+                return request.args.get('hub.challenge')
+            # Failure
+            return 'Failed!'
+        return 'Hello, world!'
     
 if __name__ == '__main__':
     app.run(debug=True)
